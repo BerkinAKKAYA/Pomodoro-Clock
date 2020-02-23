543c2ec4686e435ab216a6f3f662f3fe8fd582aa
Berkin AKKAYA <berkin_akkaya@hotmail.com>
2020-02-23 19:13:12 +0300

    Added comments

--- js/sectionManager.js
+++ js/sectionManager.js
@@ -5,15 +5,18 @@ var sectionButtons = document
 sectionButtons[0].addEventListener("click", ChangeSection)
 sectionButtons[1].addEventListener("click", ChangeSection)
 
+// Move between "Pomodoro" and "Break" sections
 function ChangeSection()
 {
     if (this.id == "active")
         return
 
+    // Disable both then enable the clicked section
     sectionButtons[0].id = "";
     sectionButtons[1].id = "";
     this.id = "active";
 
+    // Reset timer
     currentSection = this.innerHTML
     ResetTimer()
 }
